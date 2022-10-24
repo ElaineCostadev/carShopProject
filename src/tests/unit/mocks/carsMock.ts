329579db162deb95863fd14d5504ecf36be5c0ae
ElaineCosta <elainerscosta@hotmail.com>
2022-10-24 16:45:02 -0300

    :feat: incluindo os mocks

---
+++ src/tests/unit/mocks/carsMock.ts
@@ -0,0 +1,103 @@
+export const correctCar = {
+  model: 'Uno da Escada',
+  year: 1963,
+  color: 'red',
+  buyValue: 3500,
+  seatsQty: 2,
+  doorsQty: 2
+};
+
+export const correctCarWithId = {
+  _id: '6356cc60ec88e98f6fe02490',
+  __v: 0,
+  model: 'Uno da Escada',
+  year: 1963,
+  color: 'red',
+  buyValue: 3500,
+  seatsQty: 2,
+  doorsQty: 2
+};
+
+export const updatedCar = {
+  model: 'Uno da Escada',
+  year: 1966,
+  color: 'blue',
+  buyValue: 3500,
+  seatsQty: 2,
+  doorsQty: 2
+};
+
+export const coverageCar = {
+  model: 'Uno da Escada',
+  year: 1963,
+  color: 'red',
+  buyValue: 3500,
+  seatsQty: 2,
+  doorsQty: 2
+};
+
+export const noModelCar = {
+  year: 1963,
+  color: 'red',
+  buyValue: 3500,
+  seatsQty: 2,
+  doorsQty: 2
+};
+
+export const noYearCar = {
+  model: 'Uno da Escada',
+  color: 'red',
+  buyValue: 3500,
+  seatsQty: 2,
+  doorsQty: 2
+}
+
+export const noColorCar = {
+  model: 'Uno da Escada',
+  year: 1963,
+  buyValue: 3500,
+  seatsQty: 2,
+  doorsQty: 2
+}
+
+export const noBuyValueCar = {
+  model: 'Uno da Escada',
+  year: 1963,
+  color: 'red',
+  seatsQty: 2,
+  doorsQty: 2
+}
+
+export const noSeatsCar = {
+  model: 'Uno da Escada',
+  year: 1963,
+  color: 'red',
+  buyValue: 3500,
+  doorsQty: 2
+}
+
+export const noDoorsCar = {
+  model: 'Uno da Escada',
+  year: 1963,
+  color: 'red',
+  buyValue: 3500,
+  seatsQty: 2,
+}
+
+export const carSeatsLtTwo = {
+  model: 'Uno da Escada',
+  year: 1963,
+  color: 'red',
+  buyValue: 3500,
+  seatsQty: 1,
+  doorsQty: 2
+}
+
+export const carDoorsLtTwo = {
+  model: 'Uno da Escada',
+  year: 1963,
+  color: 'red',
+  buyValue: 3500,
+  seatsQty: 2,
+  doorsQty: 1
+}
