2494820e97f27a30cef06c6960c1b0d358ceaeda
ElaineCosta <elainerscosta@hotmail.com>
2022-10-24 16:45:34 -0300

    :arquivos deletados/alterados

---
+++ src/models/VehicleModel.ts
@@ -0,0 +1,7 @@
+// const vehicleSchema = new Schema<IVehicle>({
+//   model: { type: String, required: true },
+//   year: { type: Number, required: true },
+//   color: { type: String, required: true },
+//   status: { type: Boolean },
+//   buyValue: { type: Number },
+// });
