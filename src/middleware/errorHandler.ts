0b57714452b93b69223dc813b5baa09e360134f0
ElaineCosta <elainerscosta@hotmail.com>
2022-10-24 14:36:34 -0300

    :feat: criando o middleware de erro para capturar os erros da aplicação e de validação de valores inseridos pelo cliente

---
+++ src/middleware/errorHandler.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from 'express';
+import { ZodError } from 'zod';
+import { errorCatalog, ErrorTypes } from '../errors/catalogErrors';
+
+const errorHandler /* : ErrorRequestHandler  */ = (
+  error: Error | ZodError,
+  _req: Request,
+  res: Response,
+  _next: NextFunction,
+) => {
+  if (error instanceof ZodError) return res.status(400).json({ message: error.issues });
+  const createdError = errorCatalog[error.message as ErrorTypes];
+  if (createdError) {
+    return res.status(createdError.status).json(createdError.message);
+  }
+
+  console.error(error);
+  return res.status(500).json({ message: 'internal error' });
+};
+
+export default errorHandler;
