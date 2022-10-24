152b401acd0b10d96ac81fd11bd5f90722d80b10
ElaineCosta <elainerscosta@hotmail.com>
2022-10-24 14:35:01 -0300

    :feat: criando index.js e app.js para rodar a aplicacao

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,13 @@
 import express from 'express';
+import 'express-async-errors';
+import carRoute from './Routes/carRoute';
+import errorHandler from './middleware/errorHandler';
 
 const app = express();
+
+app.use(express.json());
+app.use(carRoute);
+
+app.use(errorHandler);
 
 export default app;
