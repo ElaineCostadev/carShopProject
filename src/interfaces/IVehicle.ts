adc9b2678896dbeb2eb583ae2d2b6b02ac02ebb0
ElaineCosta <elainerscosta@hotmail.com>
2022-10-24 14:41:03 -0300

    :fix: incluida as mensagens personalizadas em VechileZodSchema, para validação de valores ao inserir os dados

--- src/interfaces/IVehicle.ts
+++ src/interfaces/IVehicle.ts
@@ -1,11 +1,35 @@
 import { z } from 'zod';
 
+const CURRENTYEAR = 2022;
+
 export const vehicleZodSchema = z.object({
-  model: z.string().min(3),
-  year: z.number().positive().gt(1900).lte(2022),
-  color: z.string().min(3),
-  status: z.boolean().optional(),
-  buyValue: z.number().int(),
+  model: z.string({
+    required_error: 'model is required',
+    invalid_type_error: 'model must be a string',
+  })
+    .min(3, { message: 'Must be 3 or more characters long' }),
+
+  year: z.number({
+    required_error: 'year is required',
+    invalid_type_error: 'year must be a number',
+  })
+    .positive()
+    .gt(1900, { message: 'Must be 1900 or more' })
+    .lte(CURRENTYEAR, { message: `Must be ${CURRENTYEAR} or less` }),
+
+  color: z.string({
+    required_error: 'color is required',
+    invalid_type_error: 'color must be a string',
+  })
+    .min(3, { message: 'Must be 3 or more characters long' }),
+
+  status: z.optional(z.boolean()),
+
+  buyValue: z.number({
+    required_error: 'buyValue is required',
+    invalid_type_error: 'buyValue must be a number integer',
+  })
+    .int(),
 });
 
 export type IVehicle = z.infer<typeof vehicleZodSchema>;
