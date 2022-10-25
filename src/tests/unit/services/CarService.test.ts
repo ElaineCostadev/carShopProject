5c1f55ad198c612ae8f32f35365facea4d53a7f2
ElaineCosta <elainerscosta@hotmail.com>
2022-10-25 17:37:57 -0300

    :feat: incluindo testes das funcoes update e delete da camada service

--- src/tests/unit/services/CarService.test.ts
+++ src/tests/unit/services/CarService.test.ts
@@ -16,22 +16,14 @@ describe('Tests de Cars SERVICE', () => {
   const carModel = new CarModel();
   const carService = new CarService(carModel);
 
-  before(() => {
-    sinon.stub(carModel, 'create').resolves(carsMock.correctCarWithId);
-    sinon.stub(carModel, 'read').resolves(carsMock.readAllCars);
-    sinon.stub(carModel, 'readOne')
-    .onCall(0).resolves(carsMock.correctCarWithId)
-    .onCall(1).resolves(null);
-    // sinon.stub(carModel, 'update').resolves();
-    // sinon.stub(carModel, 'destroy').resolves();
-  });
-
-  after(()=>{
+  afterEach(()=>{
     sinon.restore();
   })
 
   describe('Inserindo(post) um novo Car', () => {
     it('Car criado com sucesso', async () => {
+      sinon.stub(carModel, 'create').resolves(carsMock.correctCarWithId);
+  
       const newCar = await carService.create(carsMock.correctCar);
       expect(newCar).to.be.deep.equal(carsMock.correctCarWithId)
     });
@@ -49,6 +41,8 @@ describe('Tests de Cars SERVICE', () => {
 
   describe('Procurando(getAll) - realAll de todos os Cars', () => {
     it('Cars encontrados com sucesso', async () => {
+      sinon.stub(carModel, 'read').resolves(carsMock.readAllCars);
+
       const allCars = await carService.read();
       expect(allCars).to.be.deep.equal(carsMock.readAllCars)
     })
@@ -56,11 +50,15 @@ describe('Tests de Cars SERVICE', () => {
 
   describe('Procurando(readOne) - readOne de um Car', () => {
     it('Apenas um Car encontrado com sucesso', async () => {
+      sinon.stub(carModel, 'readOne').resolves(carsMock.correctCarWithId)
+
       const oneCar = await carService.readOne(carsMock.correctCarWithId._id);
       expect(oneCar).to.be.deep.equal(carsMock.correctCarWithId)
     })
 
     it('Quando não é encontrado um Car ', async () => {
+      sinon.stub(carModel, 'readOne').resolves(null);
+
       let error;
       try {
         await carService.readOne(carsMock.wrongCarWithId._id);
@@ -72,8 +70,60 @@ describe('Tests de Cars SERVICE', () => {
     })
   });
 
+  describe('Atualizando(update) - um Car', () => {
+    it('Apenas um Car atualizado com sucesso', async () => {
+      sinon.stub(carModel, 'update').resolves(carsMock.updateCarWithId);
+
+      const oneCar = await carService.update(carsMock.correctCarWithId._id, carsMock.updatedCar);
+      expect(oneCar).to.be.deep.equal(carsMock.updateCarWithId)
+    })
+
+    it('Quando o obj enviado não é valido - ZodError ', async () => {
+      let error;
+      try {
+        await carService.update(carsMock.correctCarWithId._id, carsMock.noYearCar);
+      } catch (err: any) {
+        error = err;
+      }
+      // expect(error).not.to.be.undefined;
+      expect(error).to.be.instanceOf(ZodError);
+    })
+
+    it('Quando não é encontrado um Car ', async () => {
+      sinon.stub(carModel, 'update').resolves(null);
+      let error;
+      try {
+        await carService.update(carsMock.wrongCarWithId._id, carsMock.updatedCar);
+      } catch (err: any) {
+        error = err;
+      }
+      // expect(error).not.to.be.undefined;
+      expect(error.message).to.be.deep.equal(ErrorTypes.ObjectNotFound)
+    })
+  });
+
+  describe('Deletando(delete) - um Car', () => {
+    it('Apenas um Car deletado com sucesso', async () => {
+      sinon.stub(carModel, 'delete').resolves(carsMock.correctCarWithId);
+
+      const oneCar = await carService.delete(carsMock.correctCarWithId._id);
+      expect(oneCar).to.be.deep.equal(carsMock.correctCarWithId)
+    })
+
+    it('Quando não é encontrado um Car ', async () => {
+      sinon.stub(carModel, 'delete').resolves(null);
+      let error;
+      try {
+        await carService.delete(carsMock.wrongCarWithId._id);
+      } catch (err: any) {
+        error = err;
+      }
+      expect(error).not.to.be.undefined;
+      expect(error.message).to.be.deep.equal(ErrorTypes.ObjectNotFound)
+    })
+  });
+
+  
+
 });
-  // it('', async () => {});
-  // it('', async () => {});
-  // it('', async () => {});
 
