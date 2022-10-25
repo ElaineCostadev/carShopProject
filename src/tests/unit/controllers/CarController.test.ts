954b367ade64da65c18fce6eb3432adc1ac9467a
ElaineCosta <elainerscosta@hotmail.com>
2022-10-25 17:38:23 -0300

    :feat: incluindo testes das funcoes update e delete da camada controller

--- src/tests/unit/controllers/CarController.test.ts
+++ src/tests/unit/controllers/CarController.test.ts
@@ -18,6 +18,7 @@ describe('Tests de Cars CONTROLLER', () => {
   beforeEach(() => {
     res.status = sinon.stub().returns(res);
     res.json = sinon.stub().returns(res);
+    res.end = sinon.stub().returns(res);
   });
 
   afterEach(() =>{
@@ -84,6 +85,40 @@ describe('Tests de Cars CONTROLLER', () => {
 
       const jsonStub = res.json  as sinon.SinonStub;     
       expect(jsonStub.calledWith(carsMock.correctCarWithId)).to.be.true;
+    })
+  });
+
+  describe('Atualizandoi(update) - de apenas um Car', () => {
+    it('Apenas um Car encontrado com sucesso', async () => {
+
+      sinon.stub(carService, 'update').resolves(carsMock.updateCarWithId);
+      req.params = { id: carsMock.correctCarWithId._id };
+      req.body = carsMock.updatedCar;
+
+      await carController.update(req, res);
+      
+      const statusStub = res.status as sinon.SinonStub;
+      expect(statusStub.calledWith(200)).to.be.true
+
+      const jsonStub = res.json  as sinon.SinonStub;     
+      expect(jsonStub.calledWith(carsMock.updateCarWithId)).to.be.true;
+    })
+  });
+
+  describe('Deletando(delete) - de apenas um Car', () => {
+    it('Apenas um Car deletado com sucesso', async () => {
+
+      sinon.stub(carService, 'delete').resolves(carsMock.correctCarWithId);
+      req.params = { id: carsMock.correctCarWithId._id };
+
+      await carController.delete(req, res);
+      
+      const statusStub = res.status as sinon.SinonStub;
+      
+      expect(statusStub.calledWith(204)).to.be.true
+
+      // const jsonStub = res.json  as sinon.SinonStub;     
+      // expect(jsonStub.calledWith(carsMock.correctCarWithId)).to.be.true;
     })
   });
 
