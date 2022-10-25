7e40642b655af1a3913e9b6b10505db6246d41fc
ElaineCosta <elainerscosta@hotmail.com>
2022-10-25 14:56:43 -0300

    :feat: incluindo testes para a funcao readOne

--- src/tests/unit/models/CarModel.test.ts
+++ src/tests/unit/models/CarModel.test.ts
@@ -1,23 +1,27 @@
 import * as sinon from 'sinon';
 import chai from 'chai';
+import * as mongoose from 'mongoose';
 import { Model } from 'mongoose';
 import * as carsMock from '../mocks/carsMock';
 import CarModel from '../../../models/CarModel';
+import { errorCatalog, ErrorTypes } from '../../../errors/catalogErrors';
 const { expect } = chai;
 
 describe('Teste de Cars MODEL', () => {
   const carModel = new CarModel();
 
-  before(() => {
+  beforeEach(() => {
     sinon.stub(Model, 'create').resolves(carsMock.correctCarWithId);
     sinon.stub(Model, 'find').resolves(carsMock.readAllCars);
-    // sinon.stub(Model, 'find').resolves([]);
-    // sinon.stub(Model, 'findOne').resolves();
+    sinon.stub(Model, 'findOne').resolves(carsMock.correctCarWithId)
+      .onCall(0).resolves(carsMock.correctCarWithId)
+      .onCall(1).resolves(null);
+      // sinon.stub(mongoose, 'isValidObjectId').rejects();
     // sinon.stub(Model, 'findByIdAndUpdate').resolves();
     // sinon.stub(Model, 'findByIdAndDelete').resolves();
   });
 
-  after(()=>{
+  afterEach(()=>{
     sinon.restore();
   })
 
@@ -28,28 +32,33 @@ describe('Teste de Cars MODEL', () => {
     })
   });
 
-  describe('Procurando(getAll) - realAll de todos os Cars', () => {
+  describe('Procurando(readAll) - realAll de todos os Cars', () => {
     it('Cars encontrados com sucesso', async () => {
       const newCar = await carModel.read();
       expect(newCar).to.be.deep.equal(carsMock.readAllCars)
     })
   });
 
-  // describe('Procurando todos os cars(find)', () => {
-  //   it('Encontrando um array de todos os Car com sucesso', async () => {
-     
-  //   })
-  // });
+  describe('Procurando(readOne) - realOne de apenas um Car', () => {
+    it('Car encontrado com sucesso', async () => {
+      const newCar = await carModel.readOne(carsMock.correctCarWithId._id);
+      expect(newCar).to.be.deep.equal(carsMock.correctCarWithId)
+    })
 
-  // describe('Procurando Um o car(findOne)', () => {
-  //   it('Encontrando um objeto com apenas um Car com sucesso', async () => {
-     
-  //   })
+    it('Id do Car não é hexadecimal', async () => {
 
-  //   it('Quando Não é Encontrado um objeto, ID de Car valido', async () => {
-     
-  //   })
-  // });
+      let error;
+      try {
+        await carModel.readOne(carsMock.wrongCarWithId._id);
+      } catch (err: any) {      
+        error = err.message
+      }
+      expect(error).to.be.equal(ErrorTypes.InvalidMongoId)
+    })
+
+    
+  });
+
 
 
 });
