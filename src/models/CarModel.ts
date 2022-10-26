b61786430b4930025933c7526a19a091f543ef92
ElaineCosta <elainerscosta@hotmail.com>
2022-10-26 19:35:54 -0300

    :fix: ajustando a importacao na CarModel

--- src/models/CarModel.ts
+++ src/models/CarModel.ts
@@ -1,7 +1,7 @@
 import { model as mongooseCreateModel } from 'mongoose';
 import { ICar } from '../interfaces/ICar';
 import MongoModel from './MongoModel';
-import carMongooseSchema from '../utils/schemas';
+import { carMongooseSchema } from '../utils/schemas';
 
 class CarModel extends MongoModel<ICar> {
   constructor(model = mongooseCreateModel('Car', carMongooseSchema)) { 
