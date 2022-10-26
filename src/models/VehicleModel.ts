9d8aa145663465d7c68a5157551838637663bd2b
ElaineCosta <elainerscosta@hotmail.com>
2022-10-26 19:39:13 -0300

    :fix: ajustando a implementacao de CarService<ICar> - e excluindo o arquivo criado e nao utilizado

--- src/models/VehicleModel.ts
+++
@@ -1,7 +0,0 @@
-// const vehicleSchema = new Schema<IVehicle>({
-//   model: { type: String, required: true },
-//   year: { type: Number, required: true },
-//   color: { type: String, required: true },
-//   status: { type: Boolean },
-//   buyValue: { type: Number },
-// });
--- src/services/CarService.ts
+++ src/services/CarService.ts
@@ -1,9 +1,10 @@
 import { ErrorTypes } from '../errors/catalogErrors';
 import { carZodSchema, ICar } from '../interfaces/ICar';
 import { IModel } from '../interfaces/IModel';
+import IService from '../interfaces/IService';
 // import IService from '../interfaces/IService';
 
-class CarService /* implements IService<ICar>  */{
+class CarService implements IService<ICar> {
   private _car:IModel<ICar>;
   constructor(model:IModel<ICar>) {
     this._car = model;
