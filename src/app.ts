5c76bfc61dfb9a09212ce079dda82a33e8d8db9a
ElaineCosta <elainerscosta@hotmail.com>
2022-10-26 19:37:31 -0300

    :feat: incluindo e importando a rota de motorcycle no app

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import 'express-async-errors';
 import carRoute from './Routes/carRoute';
 import errorHandler from './middleware/errorHandler';
+import motorcycleRoute from './Routes/motorcycleRoute';
 
 const app = express();
 
 app.use(express.json());
 app.use(carRoute);
+app.use(motorcycleRoute);
 
 app.use(errorHandler);
 
